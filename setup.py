291bedae0c7995a3f2feb5bd6a9a74bc21c3d680
Marcus Breese <marcus.breese@ucsf.edu>
2017-04-27 13:20:02 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='swalign',
-      version='0.3.3',
+      version='0.3.4',
       description='Smith-Waterman local aligner',
       author='Marcus Breese',
       author_email='marcus@breese.com',
