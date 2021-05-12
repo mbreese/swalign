80e5b5dbc8840d7313cd1d841ae48fcfa65a2d13
jackwadden <jackwadden@gmail.com>
2021-05-12 12:41:49 -0400

    Updated setup.py to enable cythonization.

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
-from setuptools import setup
+from setuptools import setup, Extension
+from Cython.Build import cythonize
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+ext_modules = [
+    Extension(
+        r'swalign',
+        [r'swalign/__init__.py']
+    ),
+]
+    
 setup(name='swalign',
       version='0.3.6',
       description='Smith-Waterman local aligner',
@@ -10,9 +18,8 @@ setup(name='swalign',
       long_description_content_type="text/markdown",
       author='Marcus Breese',
       author_email='marcus@breese.com',
-      url='http://github.com/mbreese/swalign/',
       packages=['swalign'],
       scripts=['bin/swalign'],
       python_requires='>=3.1',
-
+      ext_modules=cythonize(ext_modules),
      )
