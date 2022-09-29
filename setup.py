d7876f2297139a36f3aaf1e52f57728893f4181f
Marcus R. Breese <marcus.breese@ucsf.edu>
2022-09-29 10:59:52 -0700

    pypi update/bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='swalign',
-      version='0.3.6',
+      version='0.3.7',
       description='Smith-Waterman local aligner',
       long_description=long_description,
       long_description_content_type="text/markdown",
